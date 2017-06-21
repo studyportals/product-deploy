51f593d3e81cdcb52a4df071c193fc3fb9618661
CZYK <danko@czyk.nl>
2017-06-21 18:07:18 +0800

    Moved the functional code inside the `it` statement.

--- test/use-strict.test.js
+++ test/use-strict.test.js
@@ -6,16 +6,18 @@ const fs = require('fs');
 const globby = require('globby');
 
 describe('Structurally', function(){
-	const nonStrictFiles = [];
-	const allFiles = globby.sync(["**/*.js", '!node_modules/**']);
-
-	allFiles.forEach(file =>{
-		if(fs.readFileSync(file).indexOf('use strict') === -1){
-			nonStrictFiles.push(file);
-		}
-	});
 
 	it('all js files should "use strict"', () =>{
+
+		const nonStrictFiles = [];
+		const allFiles = globby.sync(["**/*.js", '!node_modules/**']);
+
+		allFiles.forEach(file =>{
+			if(fs.readFileSync(file).indexOf('use strict') === -1){
+				nonStrictFiles.push(file);
+			}
+		});
+
 		expect(nonStrictFiles).to.deep.equal([]);
 	});
 });
