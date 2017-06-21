bec22d4f4ea8a623083536ce614ba7e1cc94763b
CZYK <danko@czyk.nl>
2017-06-21 10:11:50 +0800

    Removed unused line of code.

--- test/index.test.js
+++ test/index.test.js
@@ -3,12 +3,10 @@
 const chai = require('chai');
 const chaiFiles = require('chai-files');
 
-// chai.use(chaiFiles);
-
 const expect = chai.expect;
 const file = chaiFiles.file;
 
-it('index.js should exist.', () => {
+it('index.js should exist.', () =>{
 
 	expect(file('./index.js')).to.exist;
 });
