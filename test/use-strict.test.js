269a552c4b47c8a13c759988ebd4b7cc1b955889
CZYK <danko@czyk.nl>
2017-06-21 18:03:48 +0800

    Cleanup

--- test/use-strict.test.js
+++ test/use-strict.test.js
@@ -6,20 +6,16 @@ const fs = require('fs');
 const globby = require('globby');
 
 describe('Structurally', function(){
-
 	const nonStrictFiles = [];
 	const allFiles = globby.sync(["**/*.js", '!node_modules/**']);
 
 	allFiles.forEach(file =>{
-
 		if(fs.readFileSync(file).indexOf('use strict') === -1){
-
 			nonStrictFiles.push(file);
 		}
 	});
 
 	it('all js files should "use strict"', () =>{
-
 		expect(nonStrictFiles).to.deep.equal([]);
 	});
 });
