fe198fecd555832fe683aac7b952dd11dd7d161a
CZYK <danko@czyk.nl>
2017-07-07 10:27:54 +0800

    Added  an inner function to call on retry.
    @see https://cmsdk.com/javascript/how-to-retry-a-promise-in-es6-duplicate.html

--- test.js
+++ test.js
@@ -2,4 +2,16 @@ const rimraf = require('./lib/rimraf');
 const path = require('path');
 
 let dir = `${path.resolve(__dirname)}/testcases/prepare`;
-return rimraf(dir);
+return rimraf(dir)
+	.then(function() {
+
+		console.log('done');
+	})
+	.then(function() {
+
+		console.log('next');
+	})
+	.catch(function() {
+		process.exit();
+	});
+//process.exit();
