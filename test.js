0ec46c04eee3b057998fd829f2dff4ed69828162
CZYK <danko@czyk.nl>
2017-07-10 14:04:08 +0800

    Removed test case.

--- test.js
+++
@@ -1,9 +0,0 @@
-const prepare = require('./lib/prepare');
-const path = require('path');
-
-let dir = `${path.resolve(__dirname)}/testcases/prepare`;
-return prepare.emptyDir(dir)
-	.then(function() {
-
-		console.log('done');
-	});
