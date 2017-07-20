58f37095c5b86b37dc4ebfea79f4bee754f9c471
Danko Adamczyk <danko@czyk.nl>
2017-07-20 10:06:30 +0200

    v1.4.0 Added @studyportals/sass package. (#26)
    * Added @studyportals/sass package.
    
    * 1.4.0

--- index.js
+++ index.js
@@ -6,6 +6,18 @@
 
 /**
  * @static
+ * @see {@link https://www.npmjs.com/package/@studyportals/node-log|@studyportals/node-log}
+ */
+const log = require('@studyportals/node-log');
+
+/**
+ * @static
+ * @see {@link #module_lib/prepare|lib/prepare}
+ */
+const prepare = require('./lib/prepare');
+
+/**
+ * @static
  * @see {@link https://www.npmjs.com/package/@studyportals/bower|@studyportals/bower}
  */
 const bower = require('@studyportals/bower');
@@ -18,19 +30,14 @@ const composer = require('@studyportals/composer');
 
 /**
  * @static
- * @see {@link https://www.npmjs.com/package/@studyportals/node-log|@studyportals/node-log}
+ * @see {@link https://www.npmjs.com/package/@studyportals/sass|@studyportals/sass}
  */
-const log = require('@studyportals/node-log');
-
-/**
- * @static
- * @see {@link #module_lib/prepare|lib/prepare}
- */
-const prepare = require('./lib/prepare');
+const sass = require('@studyportals/sass');
 
 module.exports = {
+	log,
+	prepare,
 	bower,
 	composer,
-	log,
-	prepare
+	sass,
 };
