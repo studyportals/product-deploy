dd218202896e1d41f05350211181ea1137b1ae2b
Danko Adamczyk <danko@czyk.nl>
2017-09-06 15:04:46 +0200

    v2.0.0 (#39)
    * updated packages.
    
    * Huge typo. alpha.9
    
    * alpha.10 updated dependencies
    
    * Added the deploylogger to `product-deploy`
    
    * Log the username in the deploylog
    
    * Set the process.env vars from the Config.json file (if found).
    
    * Removed the full deploy since we do not want to support it any further.
    
    * use the PRTL_ENV var instead of options.
    
    * use the PRTL_ENV var instead of options.
    
    * Use the correct parameter.
    
    * 2.0.0

--- index.js
+++ index.js
@@ -4,19 +4,14 @@
  * @module @studyportals/product-deploy
  */
 
+require('./lib/private/config');
+
 /**
  * @static
  * @see {@link #module_lib/attachToGulp|lib/attachToGulp}
  */
 const attachToGulp = require('./lib/attachToGulp');
 
-/**
- * @static
- * @see {@link #module_lib/portal|lib/portal}
- */
-const Portal = require('./lib/deploy');
-
 module.exports = {
 	attachToGulp,
-	Portal
 };
