896cb64fb06518903ee7cd2fbc7d2c5702cb4d1d
CZYK <danko@czyk.nl>
2017-10-25 15:43:39 +0200

    Cleanup

--- bin/mocha.js
+++ bin/mocha.js
@@ -9,25 +9,27 @@ const Mocha = require('mocha'),
 // Instantiate a Mocha instance.
 
 const mocha = new Mocha({
-	reporter: path.resolve(__dirname, '..', 'lib',  'private', 'mochaReporter')
+	reporter: path.resolve(__dirname, '..', 'lib', 'private', 'mochaReporter')
 });
 
 const testDir = path.resolve(process.cwd(), 'test');
 
 // Add each .js file to the mocha instance
-fs.readdirSync(testDir).filter(function(file){
-	// Only keep the .js files
-	return file.substr(-3) === '.js';
+fs.readdirSync(testDir)
+	.filter(function(file){
+		// Only keep the .js files
+		return file.substr(-3) === '.js';
 
-}).forEach(function(file){
-	mocha.addFile(
-		path.join(testDir, file)
-	);
-});
+	})
+	.forEach(function(file){
+		mocha.addFile(
+			path.join(testDir, file)
+		);
+	});
 
 // Run the tests.
 mocha.run(function(failures){
-	process.on('exit', function () {
+	process.on('exit', function(){
 		process.exit(failures);  // exit with non-zero status if there were failures
 	});
 });
